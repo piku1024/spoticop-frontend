1e936286982aa37ba49ecebad88e19272e1ec2d2
piku1024 <piyush1024@gmail.com>
2025-03-19 04:14:43 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import UserDetails from "./components/UserDetails";
 import UserSavedAlbums from "./components/UserSavedAlbums";
 import UserPlaylists from "./components/UserPlaylists";
